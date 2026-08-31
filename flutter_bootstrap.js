9f8a5b8a71c9a982c3a717193d846d32783f4704
ait-maker <ait-maker@users.noreply.github.com>
2026-08-31 08:07:29 +0000

    deploy: 페이지 로딩 멈춤 수정 - Firebase timeout + SW 캐시 초기화

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "40956111"
+    serviceWorkerVersion: "3025017702"
   }
 });
